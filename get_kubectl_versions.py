b1ebeb07cc2c7cd518bd79746b69f50e606699ed
benoit.couloigner <benoit@les-tilleuls.coop>
2025-11-03 15:19:34 +0100

    test extract versions

---
+++ get_kubectl_versions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import os
+import json
+MIN_VERSION = 1.32
+versions = os.getenv('VERSIONS').split('\n')
+ret = {}
+for v in versions:
+    vsplit = v.split('.')
+    short_version = float(".".join([vsplit[0],vsplit[1]]).lstrip("v"))
+    if  short_version >= MIN_VERSION:
+        ret[vsplit[1]] = v
+print(json.dumps(ret))
