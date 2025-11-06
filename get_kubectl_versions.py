32b9a2e656db291739ea96d6079785f4991eb05b
benoit.couloigner <benoit@les-tilleuls.coop>
2025-11-06 13:39:15 +0100

    do not redo build

--- get_kubectl_versions.py
+++ get_kubectl_versions.py
@@ -2,7 +2,8 @@
 import os
 import json
 MIN_VERSION = 1.32
-versions = os.getenv('ALL_VERSIONS').split('\n')
+versions = os.getenv('KUBECTL_TAGS').split('\n')
+our_current_tags = os.getenv('OUR_CURRENT_TAGS').split('\n')
 ret = {}
 ret2 = []
 for v in versions:
@@ -10,5 +11,6 @@ for v in versions:
     tag = float(".".join([vsplit[0],vsplit[1]]).lstrip("v"))
     if  tag >= MIN_VERSION:
         ret[tag] = v
-print(json.dumps(ret))
-    
+todo = {k:v for k,v in ret.items() if not v in our_current_tags}
+print(json.dumps(todo))
+
