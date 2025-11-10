60bf5979ee4572592d07b2cf4d61e3db9fe0be09
benoit.couloigner <benoit@les-tilleuls.coop>
2025-11-10 13:52:45 +0100

    test multiple

--- get_kubectl_versions.py
+++ get_kubectl_versions.py
@@ -2,16 +2,18 @@
 import os
 import json
 MIN_VERSION = 1.32
-versions = os.getenv('KUBECTL_TAGS').split('\n')
-our_current_tags = os.getenv('OUR_CURRENT_TAGS').split('\n') # tags in our repository
+versions = os.getenv('KUBECTL_TAGS', '').split('\n')
+our_current_tags = os.getenv('OUR_CURRENT_TAGS', '').split('\n') # tags in our repository
+base_image= os.getenv('BASE_IMAGE' ,'')
 ret = {}  # to store k:v  with for exemple  {"1.28": "v1.28.9" ...}
 for v in versions:
     vsplit = v.split('.')
     tag = float(".".join([vsplit[0],vsplit[1]]).lstrip("v"))
     if  tag >= MIN_VERSION:
-        ret[tag] = v
+        image_tags =  ",".join([ ":".join([base_image, str(t)])  for t in [ v , v.strip('v'), tag]])
+        ret[tag] = {'base': v , 'image_tags': image_tags }
 
 # do not rebuild exiting image tag
-todo = {k:v for k,v in ret.items() if not v in our_current_tags}
+todo = {k:v for k,v in ret.items() if not v['base'] in our_current_tags}
 print(json.dumps(todo))
 
